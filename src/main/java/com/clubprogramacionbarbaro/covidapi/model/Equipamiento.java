1d0d64be02d2b64e7af13fddcc4364661724db55
Jose <jose.alba17@gmail.com>
2020-04-29 20:52:38 -0500

    Swagger added

--- src/main/java/com/clubprogramacionbarbaro/covidapi/model/Equipamiento.java
+++ src/main/java/com/clubprogramacionbarbaro/covidapi/model/Equipamiento.java
@@ -10,24 +10,46 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Entity
 @Table(name = "equipamiento")
 @Data
+@ApiModel(description="All details about Equipamiento")
 public class Equipamiento {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer equipamientoId;
+	
 	@Column(nullable = true)
+	@Positive
 	private Integer hospitalId; //Llave foránea
+	
+	@Size(min= 15, message="Nombre should have at least 15 characters")
+	@ApiModelProperty(notes="Nombre should have at least 15 characters")
 	private String nombre;
+	
+	@Size(min= 15, message="Descripcion should have at least 15 characters")
+	@ApiModelProperty(notes="Descripcion should have at least 15 characters")
 	private String descripcion;
+	
+	@Positive
+	@NotBlank
+	@ApiModelProperty(notes="Cantidad should have at least 15 characters")
 	private Integer cantidad;
-
+	
+	@Future
+	@NotBlank
 	@Temporal(TemporalType.DATE)
+	@ApiModelProperty(notes="fechaVencimiento should be a future date")
 	private Date fechaVencimiento;
 	
 	@Temporal(TemporalType.TIMESTAMP)
--- src/main/java/com/clubprogramacionbarbaro/covidapi/model/Hospital.java
+++ src/main/java/com/clubprogramacionbarbaro/covidapi/model/Hospital.java
@@ -14,21 +14,42 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Entity
 @Table(name = "hospital")
 @Data
+@ApiModel(description="All details about Hospital")
 public class Hospital {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer hospitalId;
+	
+	@NotBlank
+	@Size(min= 15, message="Nombre should have at least 15 characters")
+	@ApiModelProperty(notes="Nombre should have at least 15 characters")
 	private String nombre;
+	
+	@NotBlank
+	@ApiModelProperty(notes="latitud should not be blank")
 	private Double latitud;
+	
+	@NotBlank
+	@ApiModelProperty(notes="longitud should not be blank")
 	private Double longitud;
+	
+	@Size(min= 15, message="tipoInstitucion should have at least 15 characters")
+	@ApiModelProperty(notes="tipoInstitucion should have at least 15 characters")
 	private String tipoInstitucion;
+	
+	@Size(min= 15, message="Domicilio should have at least 15 characters")
+	@ApiModelProperty(notes="Domicilio should have at least 15 characters")
 	private String domicilio;
 
 	@Temporal(TemporalType.TIMESTAMP)
