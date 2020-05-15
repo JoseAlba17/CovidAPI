0de0e8aad1924bc8d06d14b23d74905e05efb810
Jose <jose.alba17@gmail.com>
2020-05-15 02:25:31 -0500

    Swagger with token security

--- src/main/java/com/clubprogramacionbarbaro/covidapi/config/SwaggerConfig.java
+++ src/main/java/com/clubprogramacionbarbaro/covidapi/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.clubprogramacionbarbaro.covidapi.config;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -9,17 +8,13 @@ import java.util.Set;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.service.SecurityReference;
-import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
