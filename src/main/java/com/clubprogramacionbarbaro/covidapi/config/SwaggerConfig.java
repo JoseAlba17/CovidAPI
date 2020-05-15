4772f446160ef8393bbd20cdfa025ec4b34c2f48
Jose <jose.alba17@gmail.com>
2020-05-15 02:19:08 -0500

    Swagger with auth token

--- src/main/java/com/clubprogramacionbarbaro/covidapi/config/SwaggerConfig.java
+++ src/main/java/com/clubprogramacionbarbaro/covidapi/config/SwaggerConfig.java
@@ -1,17 +1,27 @@
 package com.clubprogramacionbarbaro.covidapi.config;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -19,24 +29,65 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(
-			Arrays.asList("application/json"/*, "application/xml"*/));
-
-	private static Contact DEFAULT_CONTACT = new Contact("José María Alba", "", "jose.alba17@gmail.com");
-
-	private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("CovidApi Documentation",
-			"This api helps the hospitals to always have the necessary material during the Covid-19 pandemy", "1.0",
-			"urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licences/LICENSE-2.0", new HashSet<>());
+	private static final String HEADER = "header";
+	private static final String STRING = "String";
 
 	@Bean
 	public Docket api() {
 
-		return new Docket(DocumentationType.SWAGGER_2).select()
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.clubprogramacionbarbaro.covidapi"))
 				.paths(PathSelectors.any()).build()
-				.apiInfo(DEFAULT_API_INFO)
-				.forCodeGeneration(true)
-				.produces(DEFAULT_PRODUCES_AND_CONSUMES)
-				.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+				.apiInfo(apiInfo()).forCodeGeneration(true)
+				.produces(defaultProducesAndConsumes())
+				.consumes(defaultProducesAndConsumes())
+				.securitySchemes(securitySchemes())
+				.securityContexts(securityContext());
+	}
+
+	private List<SecurityContext> securityContext() {
+		return Arrays.asList(SecurityContext.builder()
+				.securityReferences(defaultAuth())
+				.forPaths(PathSelectors.regex("/v1.*"))
+				.build());
+	}
+	
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("Bearer", authorizationScopes));
+	}
+	
+	private List<ApiKey> securitySchemes() {
+		return Arrays.asList(new ApiKey("Bearer", "Authorization", "header"));
+	}
+	
+	private ApiInfo apiInfo() {
+
+		return new ApiInfo("CovidApi Documentation",
+				"This api helps the hospitals to always have the necessary material during the Covid-19 pandemy", "1.0",
+				"urn:tos", defaultContact(), "Apache 2.0", "http://www.apache.org/licences/LICENSE-2.0",
+				new HashSet<>());
+	}
+
+	private Contact defaultContact() {
+
+		return new Contact("José María Alba", "", "jose.alba17@gmail.com");
+	}
+
+	/*private List<Parameter> getGobalParameters() {
+		List<Parameter> params = new ArrayList<Parameter>();
+
+		params.add(new ParameterBuilder().name("Authorization Bearer").description("Authorization")
+				.modelRef(new ModelRef(STRING)).parameterType(HEADER).required(false).build());
+
+		return params;
+	}*/
+
+	private Set<String> defaultProducesAndConsumes() {
+
+		return new HashSet<String>(Arrays.asList("application/json"/* , "application/xml" */));
 	}
 }
